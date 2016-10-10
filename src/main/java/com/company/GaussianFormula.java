7d887638e2b58eac47ab152862bc1ea880f0d3af
Jessie Ding <nathalie.ding@gmail.com>
2016-10-10 17:18:58 +0800

    changed

--- src/main/java/com/company/GaussianFormula.java
+++ src/main/java/com/company/GaussianFormula.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Gaussian {
+public class GaussianFormula {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
