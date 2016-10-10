2e7bd79340d8e26cb648c645d8782b0a10e18bfb
Jessie Ding <nathalie.ding@gmail.com>
2016-10-10 16:53:40 +0800

    init project

---
+++ src/main/java/com/company/Gaussian.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Gaussian {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("n=（请输入大于等于4的整数）");
+        String num = scan.nextLine();
+        int n = Integer.parseInt(num);
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("1 + 2 + 3 + ··· + n ="+sum);
+    }
+}
